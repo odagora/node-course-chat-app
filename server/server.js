2657d8d730a58374fa63f59a5dd8492df8af6283
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-06-22 18:47:02 -0500

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,32 @@
 //We require the 'path' nodejs method to set the public path.See: 'https://nodejs.org/dist/latest-v8.x/docs/api/path.html':
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
+const socketIO = require('socket.io');
 // console.log(publicPath);
 
 //We import express framework to create the server:
 const express = require('express');
 var app = express();
+//To create a server that uses 'socket.io', we need to add a new server using the 'http' node.js method:
+var server = http.createServer(app);
+//Then we pass the server to the 'socketIO' variable:
+var io = socketIO(server);
 
 //We define to use the 'public' folder as a static asset:
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+//We register an event listener to use with 'socket.io'. In this case when a connection is established:
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  //We register another event that fires when the user closes the tab/window with the connection:
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+//We change the 'app' variable to 'server':
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
